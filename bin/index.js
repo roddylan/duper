444c485f37937dbdc797777bfa2694a40c8cfbec
Roderick Lan <rodericklan03@gmail.com>
2025-04-01 09:33:28 -0600

    fixed demand

--- bin/index.js
+++ bin/index.js
@@ -6,13 +6,13 @@ const yargs = require("yargs");
 const options = yargs
     .usage("Usage: $0 -f <file> [-n <duplicate name>]")
     .option("f", {
-        alias:"file", describe:"File to duplicate", type:"string", demandOption:"true"
+        alias:"file", describe:"File to duplicate", type:"string", demandOption:true
     })
-    .option("-n", {
-        alias:"name", describe:"Set duplicate name", type:"string", demandOption:"false"
+    .option("n", {
+        alias:"name", describe:"Set duplicate name", type:"string", demandOption:false
     })
     .help(true)
     .argv;
 
 
-
+console.log(options)
